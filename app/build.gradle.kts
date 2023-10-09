95183783396068b30081817aa2890a08a8304242
Arfin Hosain <developerarfin@gmail.com>
2023-10-09 07:41:45 +0600

    Changed CompiledSdk from 33 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.firebasenoteapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.firebasenoteapp"
